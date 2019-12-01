e6909f5b5424ef678eef691169acf48390e5757b
Alex Heist <dev@alexheist.com>
2019-12-01 10:51:22 -0700

    Add docker configuration for local development - Basic project structure

---
+++ backend/config/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
