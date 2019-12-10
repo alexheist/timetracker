7bd5b10fd0eb33ed4f1c3655358cddac6c9a894c
Alex Heist <a.heist@alexheist.com>
2019-12-09 20:56:06 -0700

    Finish Base Landing Styles - Desktop only, will need to adjust in future - Need to add copy

--- frontend/src/base/landing.js
+++ frontend/src/base/landing.js
@@ -7,19 +7,21 @@ function Landing() {
         <>
             <Nav />
             <div id="page-container">
-                <h1>Take Control of Your Time</h1>
-                <div className="card">
-                    <h2>Useful Reports</h2>
-                    <p></p>
-                </div>
-                <div className="card">
-                    <h2>Invoice Generation</h2>
-                    <p></p>
-                </div>
-                <div className="card">
-                    <h2>Simple Management</h2>
-                    <p></p>
-                </div>
+                <h1>Take <em>Control</em> of Your Time</h1>
+                <ul id="copy">
+                    <div className="card">
+                        <h2>Useful Reports</h2>
+                        <p>Something</p>
+                    </div>
+                    <div className="card">
+                        <h2>Invoice Generation</h2>
+                        <p>To Put</p>
+                    </div>
+                    <div className="card">
+                        <h2>Simple Management</h2>
+                        <p>For Render</p>
+                    </div>
+                </ul>
             </div>
         </>
     );
