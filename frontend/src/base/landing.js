f9dfeff3e98cb1de76f191f7b8a6737f95a06da7
Alex Heist <a.heist@alexheist.com>
2019-12-09 17:48:33 -0700

    Add initial styles - Incomplete - May need to rethink the HTML

--- frontend/src/base/landing.js
+++ frontend/src/base/landing.js
@@ -3,24 +3,26 @@ import Nav from './nav';
 import './landing.css';
 
 function Landing() {
-	return (
-		<div id="page-container">
-			<Nav />
-			<h1>Take Control of Your Time</h1>
-			<div className="card">
-				<h2>Useful Reports</h2>
-				<p></p>
-			</div>
-			<div className="card">
-				<h2>Invoice Generation</h2>
-				<p></p>
-			</div>
-			<div className="card">
-				<h2>Simple Management</h2>
-				<p></p>
-			</div>
-		</div>
-	);
+    return (
+        <>
+            <Nav />
+            <div id="page-container">
+                <h1>Take Control of Your Time</h1>
+                <div className="card">
+                    <h2>Useful Reports</h2>
+                    <p></p>
+                </div>
+                <div className="card">
+                    <h2>Invoice Generation</h2>
+                    <p></p>
+                </div>
+                <div className="card">
+                    <h2>Simple Management</h2>
+                    <p></p>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Landing;
--- frontend/src/base/nav.js
+++ frontend/src/base/nav.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Login from '../auth/login';
 import Signup from '../auth/signup';
+import logo from './logo.png';
 
 function Nav() {
-	return (
-		<nav>
-			<h2>Time Tracker</h2>
-			<a href="#">Home</a>
-			<Login />
-			<Signup />
-		</nav>
-	);
+    return (
+        <nav>
+            <img src={logo} alt="Time Tracker" />
+            <h2>Time Tracker</h2>
+            <ul className={"nav-links"}>
+                <a href="#">Home</a>
+                <Login />
+                <Signup />
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
